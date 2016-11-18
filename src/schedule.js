c8a77106a50f310df086219c7d842bce013ea0d9
hallgrimur1471 <hallgrimur1471@gmail.com>
2016-11-18 18:59:02 +0000

    Use code from Olafur

---
+++ src/schedule.js
@@ -0,0 +1,33 @@
+
+const axios = require('axios');
+
+const baseURL = ''; // Sækja úr environment breytu
+const instance = axios.create({ baseURL })
+
+/**
+ * Fetches all available channels from endpoint, returns a promise that when
+ * resolved returns an array, e.g.:
+ * [{ name: 'Rúv', endpoint: '/tv/ruv' }, ... ]
+ *
+ * @returns {Promise} - Promise with available channels when resolved
+ */
+function channels() {
+
+}
+
+/**
+ * Fetches schedule for a channel by name, returns an array, e.g.:
+ * [{ title: '...', duration: '...', startTime: '...', ...}, ...]
+ * If the channel does not exist, the empty array is returned.
+ *
+ * @param {string} name - Name of the channel
+ * @returns {Promise} - Promise with schedule for channel when resolved
+ */
+function channel(name) {
+
+}
+
+module.exports = {
+  channels,
+  channel
+};
